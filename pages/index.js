e9531f87d4737a3657e7fd55d65a2ec6af75c940
pryoh <ryancm1170@gmail.com>
2023-02-08 04:09:34 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import styles from "../styles/Home.module.css";
+import { useMemo, useState, useEffect } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { MetaplexProvider } from "../MetaplexProvider";
+import { MintNFTs } from "../MintNFTs";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import dynamic from 'next/dynamic';
+
+export default function Home() {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+
+  const ButtonWrapper = dynamic(() =>
+    import('@solana/wallet-adapter-react-ui').then((mod) => mod.WalletMultiButton)
+  );
+
+
+  return (
+    <div>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <MetaplexProvider>
+              <div className={styles.App}>
+                <ButtonWrapper />
+                <MintNFTs />
+              </div>
+            </MetaplexProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
+}
